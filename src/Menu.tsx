d6cace018b7c4216edc8d99bee788436a1ef56d6
anguaive <angurg98@gmail.com>
2020-09-18 16:06:20 +0200

    Extract menu links into array
    Hide menu when game is unpaused, with a transition

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,30 +1,58 @@
 import React from 'react';
+import { Transition } from 'react-transition-group';
 import { Link } from 'react-router-dom';
 
 // TODO: :focus on keyboard events only
 // TODO: better transition
 
-const Menu = () => {
+interface NavLink {
+    path: string;
+    name: string;
+}
+
+const links: NavLink[] = [
+    { path: '/game', name: 'Play' },
+    { path: '/matchmaking', name: 'Matchmaking' },
+    { path: '/profile', name: 'Profile' },
+    { path: '/rankings', name: 'Rankings' },
+    { path: '/settings', name: 'Settings' },
+];
+
+interface MenuProps {
+    paused: boolean;
+}
+
+const defaultStyle = {
+    transition: 'opacity 100ms ease-in-out',
+    opacity: 0,
+};
+
+const transitionStyles = {
+    entering: { opacity: 1 },
+    entered: { opacity: 1 },
+    exiting: { opacity: 0 },
+    exited: { opacity: 0 },
+    unmounted: { opacity: 0 },
+};
+
+const Menu = ({ paused }: MenuProps) => {
     return (
-        <nav id="menu">
-            <ul>
-                <li>
-                    <Link to="/game">Play</Link>
-                </li>
-                <li>
-                    <Link to="/matchmaking">Matchmaking</Link>
-                </li>
-                <li>
-                    <Link to="/profile">Profile</Link>
-                </li>
-                <li>
-                    <Link to="/rankings">Rankings</Link>
-                </li>
-                <li>
-                    <Link to="/settings">Settings</Link>
-                </li>
-            </ul>
-        </nav>
+        <Transition in={paused} timeout={100}>
+            {(state) => (
+                <nav
+                    id="menu"
+                    style={{ ...defaultStyle, ...transitionStyles[state] }}
+                >
+                    <ul>
+                        {links.map((route, i) => (
+                            <li key={i}>
+                                <Link to={route.path}>{route.name}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            )}
+        </Transition>
     );
 };
 
