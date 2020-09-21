ac47269d563a0c3fd8be0203f615cbde873761e6
anguaive <angurg98@gmail.com>
2020-09-21 17:30:31 +0200

    Add aliases and top performances list

--- src/profile/Profile.tsx
+++ src/profile/Profile.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './Profile.css';
 import { ReactComponent as ExpandIcon } from '../res/chevron-circle-down.svg';
+import { ReactComponent as ClockIcon } from '../res/clock-o.svg';
+import { formatHours, formatTime } from '../utils';
 
 interface Performance {
     title: string;
@@ -45,83 +47,147 @@ const Profile = () => {
     }, []);
 
     return (
-        <main id="profile">
-            <section id="user-info">
-                <span className="container-title">User information</span>
-                <div id="user-info-grid" className="container">
-                    <img
-                        src="https://www.eatliver.com/wp-content/uploads/2019/01/tank-cat10.jpg"
-                        className="user-info__picture"
-                        alt="User"
-                    />
-                    <div className="user-info__username">Username</div>
-                    <div className="user-info__aliases">
-                        <div className="user-info__alias">Username</div>
-                        <div className="user-info__alias">Username_QWERTY</div>
-                        <div className="user-info__alias alias-active">
-                            Username_123
+        <>
+            {profile !== undefined ? (
+                <main id="profile">
+                    <section id="user-info">
+                        <span className="container-title">
+                            User information
+                        </span>
+                        <div id="user-info-grid" className="container">
+                            <img
+                                src={profile.picture}
+                                className="user-info__picture"
+                                alt="User"
+                            />
+                            <div className="user-info__username">
+                                {profile.name}
+                            </div>
+                            <div className="user-info__aliases">
+                                {profile.aliases.map((alias, i) => (
+                                    <button
+                                        key={i}
+                                        onClick={() => {
+                                            setSelectedAlias(i);
+                                            console.log(profile.aliases[i]);
+                                        }}
+                                        className={
+                                            'user-info__alias' +
+                                            (i === selectedAlias
+                                                ? ' alias-active'
+                                                : '')
+                                        }
+                                    >
+                                        {alias.name}
+                                    </button>
+                                ))}
+                            </div>
+                            <div className="user-info__stats">
+                                <div className="user-info__stat">
+                                    {profile.since}{' '}
+                                    <span className="unit">since</span>
+                                </div>
+                                <div className="user-info__stat">
+                                    {formatHours(
+                                        profile.aliases[selectedAlias].time || 0
+                                    )}{' '}
+                                    <span className="unit">hours</span>
+                                </div>
+                                <div className="user-info__stat">
+                                    {profile.aliases[selectedAlias].points}{' '}
+                                    <span className="unit">points</span> ( #xyz
+                                    )
+                                </div>
+                                <div className="user-info__stat">
+                                    {profile.aliases[selectedAlias].wpm.toFixed(
+                                        2
+                                    )}{' '}
+                                    <span className="unit">wpm</span>
+                                </div>
+                                <div className="user-info__stat">
+                                    {profile.aliases[selectedAlias].acc.toFixed(
+                                        2
+                                    )}{' '}
+                                    <span className="unit">acc</span>
+                                </div>
+                            </div>
+                            <div className="user-info__bio">{profile.bio}</div>
+                            <div className="user-info__expand">
+                                <ExpandIcon />
+                            </div>
                         </div>
-                    </div>
-                    <div className="user-info__stats">
-                        <div className="user-info__stat">
-                            2020-09-02 <span className="unit">since</span>
+                    </section>
+                    <section id="top-performances">
+                        <span className="container-title">
+                            Top performances
+                        </span>
+                        <div className="performance-container">
+                            {profile.aliases[selectedAlias].topPerformances.map(
+                                (perf, i) => (
+                                    <div className="performance">
+                                        <div className="performance__title">
+                                            {perf.title}
+                                        </div>
+                                        <div className="performance__section--when">
+                                            <div className="performance__section">
+                                                {perf.section}
+                                            </div>
+                                            <div className="performance__when">
+                                                {new Date(
+                                                    perf.when
+                                                ).toDateString()}
+                                            </div>
+                                        </div>
+                                        <div className="performance__stats">
+                                            <div>
+                                                <div className="icon-container">
+                                                    <div className="icon">
+                                                        <ClockIcon />
+                                                    </div>
+                                                </div>
+                                                <span>
+                                                    {formatTime(perf.time)}
+                                                </span>
+                                            </div>
+                                            <div>
+                                                {perf.points}
+                                                <span className="unit">
+                                                    pts
+                                                </span>
+                                            </div>
+                                            <div>
+                                                {perf.wpm.toFixed(2)}
+                                                <span className="unit">
+                                                    wpm
+                                                </span>
+                                            </div>
+                                            <div>
+                                                {perf.acc.toFixed(2)}
+                                                <span className="unit">
+                                                    acc
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            )}
                         </div>
-                        <div className="user-info__stat">
-                            3.56 <span className="unit">hours</span>
-                        </div>
-                        <div className="user-info__stat">
-                            834 <span className="unit">points</span> ( #323 )
-                        </div>
-                        <div className="user-info__stat">
-                            102.32 <span className="unit">wpm</span>
-                        </div>
-                        <div className="user-info__stat">
-                            99.12 <span className="unit">acc</span>
-                        </div>
-                    </div>
-                    <div className="user-info__bio">
-                        <strong>Hello there! I'm Username.</strong>
-                        <br />
-                        <br />
-                        Rina istorlle sinome amin uuma malia lle vesta
-                        Astalderea. Amin vasa lle sinta lema ed' ando en' templa
-                        Dinaerea Narvinye. Silmataurea ho yala onna en' kemen
-                        lle ume quel. Yala onna en' vilya tenna' tul're san'
-                        poika tuulo' 'kshapsae Hallaerea. Menelya aman tel'
-                        seldarine Yaaraerea ro. Tanka tel' taurnin Morsul Alduya
-                        Nadorhuanrim. Seasamin Laara'tincoras Unguerea amin uuma
-                        malia. Tenna' ento lye omenta tessa sina ten' amin malia
-                        ten' vasa quel fara. Amin uuma malia cormamin niuve
-                        tenna' ta elea lle au' Tulien Faradome.
-                        <br />
-                        <br />
-                        I'quelin Mori'Quessier naa ba Mori'Quessir nurta i' fea
-                        amin uuma malia cormlle naa tanya tel'raa. Ta naa
-                        seasamin neuma en' templa tenna' tul're san' tanka tel'
-                        taurnin. Usquenerea Dina faina templa amin weera yassen
-                        lle. Mani uma lle merna ten' ta Elandili Cairbara
-                        Mith'quessir. Dina quel undome Naugrim asca melloneamin.
-                        Naugiaur tua amin! re Agaryulnaerea.
-                    </div>
-
-                    <div className="user-info__expand">
-                        <ExpandIcon />
-                    </div>
-                </div>
-            </section>
-            <section id="top-performances">
-                <span className="container-title">Top performances</span>
-                <div className="container"></div>
-            </section>
-            <section id="recent-performances">
-                <span className="container-title">Recent performances</span>
-                <div className="container"></div>
-            </section>
-            <section id="performance-graph">
-                <span className="container-title">Performance graph</span>
-                <div className="container red"></div>
-            </section>
-        </main>
+                    </section>
+                    <section id="recent-performances">
+                        <span className="container-title">
+                            Recent performances
+                        </span>
+                        <div className="container"></div>
+                    </section>
+                    <section id="performance-graph">
+                        <span className="container-title">
+                            Performance graph
+                        </span>
+                        <div className="container"></div>
+                    </section>
+                </main>
+            ) : null}
+        </>
     );
 };
 
