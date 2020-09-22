c109ad40e85515bad0b4b72618617a0076cb980b
anguaive <angurg98@gmail.com>
2020-09-22 09:29:02 +0200

    Add performance card background animation

--- src/profile/PerformanceCard.tsx
+++ src/profile/PerformanceCard.tsx
@@ -6,33 +6,35 @@ import { formatTime } from '../utils';
 const PerformanceCard = (perf: Performance) => {
     return (
         <div className="performance">
-            <div className="performance__title">{perf.title}</div>
-            <div className="performance__section--when">
-                <div className="performance__section">{perf.section}</div>
-                <div className="performance__when">
-                    {new Date(perf.when).toDateString()}
-                </div>
-            </div>
-            <div className="performance__stats">
-                <div>
-                    <div className="icon-container">
-                        <div className="icon">
-                            <ClockIcon />
-                        </div>
+            <div className="performance-animation">
+                <div className="performance__title">{perf.title}</div>
+                <div className="performance__section--when">
+                    <div className="performance__section">{perf.section}</div>
+                    <div className="performance__when">
+                        {new Date(perf.when).toDateString()}
                     </div>
-                    <span>{formatTime(perf.time)}</span>
                 </div>
-                <div>
-                    {perf.points}
-                    <span className="unit">pts</span>
-                </div>
-                <div>
-                    {perf.wpm.toFixed(2)}
-                    <span className="unit">wpm</span>
-                </div>
-                <div>
-                    {perf.acc.toFixed(2)}
-                    <span className="unit">acc</span>
+                <div className="performance__stats">
+                    <div>
+                        <div className="icon-container">
+                            <div className="icon">
+                                <ClockIcon />
+                            </div>
+                        </div>
+                        <span>{formatTime(perf.time)}</span>
+                    </div>
+                    <div>
+                        {perf.points}
+                        <span className="unit">pts</span>
+                    </div>
+                    <div>
+                        {perf.wpm.toFixed(2)}
+                        <span className="unit">wpm</span>
+                    </div>
+                    <div>
+                        {perf.acc.toFixed(2)}
+                        <span className="unit">acc</span>
+                    </div>
                 </div>
             </div>
         </div>
