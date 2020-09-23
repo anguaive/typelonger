f74c254d5fba11e7c02fae764a6682cae8d869bf
anguaive <angurg98@gmail.com>
2020-09-23 15:17:01 +0200

    Add link to profile page on user card

--- src/rankings/UserCard.tsx
+++ src/rankings/UserCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { UserListView } from './Rankings';
 import { formatHours } from '../utils';
+import { Link } from 'react-router-dom';
 import './UserCard.css';
 
 const UserCard = ({
@@ -10,30 +11,34 @@ const UserCard = ({
     user: UserListView;
     placement: number;
 }) => {
+    const toProfile = () => {};
+
     return (
-        <div className="user-card">
-            <div className="user-card__placement">#{placement} </div>
-            <div className="user-card__alias">{user.aliasname} </div>
-            <div className="user-card__username">({user.username})</div>
-            <div className="user-card__stats-container">
-                <div>
-                    {formatHours(user.time)}
-                    <div className="unit">hours</div>
-                </div>
-                <div>
-                    {user.points}
-                    <div className="unit">points</div>
-                </div>
-                <div>
-                    {user.wpm.toFixed(2)}
-                    <div className="unit">wpm</div>
-                </div>
-                <div>
-                    {user.acc.toFixed(2)}
-                    <div className="unit">acc</div>
+        <Link to={'/profile/' + user.username + '/' + user.aliasname}>
+            <div className="user-card" onClick={() => toProfile()}>
+                <div className="user-card__placement">#{placement} </div>
+                <div className="user-card__alias">{user.aliasname} </div>
+                <div className="user-card__username">({user.username})</div>
+                <div className="user-card__stats-container">
+                    <div>
+                        {formatHours(user.time)}
+                        <div className="unit">hours</div>
+                    </div>
+                    <div>
+                        {user.points}
+                        <div className="unit">points</div>
+                    </div>
+                    <div>
+                        {user.wpm.toFixed(2)}
+                        <div className="unit">wpm</div>
+                    </div>
+                    <div>
+                        {user.acc.toFixed(2)}
+                        <div className="unit">acc</div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
