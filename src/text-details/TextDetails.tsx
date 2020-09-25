2a8ef88ff852cc3b6bebed1622743563fdd709d6
anguaive <angurg98@gmail.com>
2020-09-25 08:33:02 +0200

    Add empty text-details component

---
+++ src/text-details/TextDetails.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TextDetails.css';
+
+const TextDetails = () => {
+    return (
+        <main id="text-details">
+            <section id="text-info"></section>
+            <section id="text-sections"></section>
+            <section id="text-best-performances"></section>
+            <section id="your-best-performance"></section>
+        </main>
+    );
+};
+
+export default TextDetails;
