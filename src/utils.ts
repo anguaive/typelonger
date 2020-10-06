65de67710a4e1852414ef05144c8f567849b53e8
anguaive <angurg98@gmail.com>
2020-10-06 13:22:53 +0200

    Specify possible actions for all card types

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,66 @@
+import { Alias, Text, Performance, Section } from './scheme';
+
+export interface Action {
+    text: string;
+    handler: Function;
+}
+
+export const getUserActions = (alias: Alias) => [
+    {
+        text: 'View profile',
+        handler: () => {},
+    },
+];
+
+export const getTextActions = (text: Text) => [
+    {
+        text: 'Expand',
+        handler: () => {},
+    },
+    {
+        text: 'View details',
+        handler: () => {},
+    },
+];
+
+export const getSectionActions = (section: Section) => [
+    {
+        text: 'View details',
+        handler: () => {},
+    },
+    {
+        text: 'To lobby',
+        handler: () => {},
+    },
+    {
+        text: 'Play',
+        handler: () => {},
+    },
+];
+
+export const getPerfActions = (perf: Performance, location?: any) => {
+    console.log(location);
+    const actions = [
+        {
+            text: 'View replay',
+            handler: () => {},
+        },
+        {
+            text: 'Race',
+            handler: () => {},
+        },
+    ];
+
+    if (location && (location.pathname.split('/')[1] = 'text')) {
+        actions.splice(0, 0, {
+            text: 'View profile',
+            handler: () => {},
+        });
+    }
+
+    return actions;
+};
+
 // Format milliseconds to hours
 export const formatHours = (time: number): string => {
     return (time / 1000 / 60 / 60).toFixed(2);
