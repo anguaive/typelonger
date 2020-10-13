327dbc4b0e22bf007ca7c3addbe48e9e9de4cdac
anguaive <angurg98@gmail.com>
2020-10-13 22:25:49 +0200

    Display placeholder text in game component

--- src/game/Game.tsx
+++ src/game/Game.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Game.css';
 import QuickStats from './QuickStats';
 
@@ -7,18 +7,43 @@ interface GameProps {
     setPaused: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+interface QuickStats {
+    time?: number;
+    wpm?: number;
+    acc?: number;
+}
+
 const Game = ({ paused, setPaused }: GameProps) => {
-    const time = 254123,
-        wpm = 90,
-        acc = 98.56;
+    const [textParagraphs, setTextParagraphs] = useState<string[]>([]);
+    const [quickStats, setQuickStats] = useState<QuickStats>({
+        time: 254123,
+        wpm: 90,
+        acc: 98.56,
+    });
+
+    useEffect(() => {
+        fetch('http://localhost:3001/gameText')
+            .then((response) => response.json())
+            .then((data) => setTextParagraphs(data.paragraphs));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <main id="game">
-            <QuickStats time={time} wpm={wpm} acc={acc} paused={paused} />
-            <section
-                id="text-area"
-                onClick={() => setPaused(!paused)}
-            ></section>
+            <QuickStats
+                time={quickStats.time || 0}
+                wpm={quickStats.wpm || 0}
+                acc={quickStats.acc || 0}
+                paused={paused}
+            />
+            <section id="text-area" onClick={() => setPaused(!paused)}>
+                <div className="text-container">
+                    {textParagraphs.map((pg) => (
+                        <div className="pg">{pg}</div>
+                    ))}
+                    <div className="caret" />
+                </div>
+            </section>
             <section id="scorebar" className={paused ? '' : 'hidden'}>
                 scorebar
             </section>
